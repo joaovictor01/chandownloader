3a42625d385c3e5fc294fe00f6995ddf00af236e
Joao Victor <joao_victor-321@hotmail.com>
2021-02-25 14:44:30 -0300

    update

--- chandownloader.py
+++ chandownloader.py
@@ -10,22 +10,35 @@ import urllib.request, urllib.error, urllib.parse
 
 thread_number = ""
 
+def get_json_url(url):
+    return f"{url.replace('boards.4chan','a.4cdn')}.json"
+
+
+def get_thread_title(url):
+    thread_json_url = get_json_url(url)
+    r = requests.get(thread_json_url, verify=False)
+    thread_json = r.json()
+    thread_title = thread_json['posts'][0]['semantic_url']
+    return thread_title
+
+
 # Create an Image Folder for the media of the thread to be stored
-def create_img_folder(url):
+def create_img_folder(url, thread_title):
     global thread_number
     board = url.split("/")[3]
     thread_number = str(url.split("thread/")[1])
-    path = Path.joinpath(Path.home(), f'4chan/{board}/{thread_number}')
+    path = Path.joinpath(Path.home(), f'4chan/{board}/{thread_title}__{thread_number}')
     Path(path).mkdir(parents=True, exist_ok=True)
-    print(f'\nCaminho da pasta criada é: {path}')
+    print(f'\nPath of created folder is: {path}')
     return path
 
 
 def download_thread_json(url, path):
-    thread_json_url = f"{url.replace('boards.4chan','a.4cdn')}.json"
+    thread_json_url = get_json_url(url)
     print(thread_json_url)
     wget.download(thread_json_url, str(path))
     print("Thread's json downloaded successfully!")
+
 
 def download_all(url, path):
     global thread_number
@@ -68,8 +81,9 @@ def download_all(url, path):
 
 def main():
     url = input("Insert the thread link here: ")
-    path = create_img_folder(url)
-    download_all(url,path)
+    thread_title = get_thread_title(url)
+    path = create_img_folder(url, thread_title)
+    download_all(url, path)
 
 
 if __name__ == "__main__":
