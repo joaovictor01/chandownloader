3e58c09d71192d700dd73e3ca7005e6fcca41447
Joao Victor <joao_victor-321@hotmail.com>
2021-04-23 14:56:55 -0300

    update

--- chandownloader.py
+++ chandownloader.py
@@ -12,43 +12,48 @@ import threading
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-URL = ''
-PATH = ''
-THREAD_MONITORING = ''
+URL = ""
+PATH = ""
+THREAD_MONITORING = ""
 DOWNLOADING = False
 thread_number = ""
+
+
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-EXTENSIONS = ['.jpg', '.jpeg', '.png', '.gif', '.webm']
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
 
 
-class setInterval :
-    def __init__(self,interval,action) :
-        self.interval=interval
-        self.action=action
-        self.stopEvent=threading.Event()
-        thread=threading.Thread(target=self.__setInterval)
+EXTENSIONS = [".jpg", ".jpeg", ".png", ".gif", ".webm"]
+
+
+class setInterval:
+    def __init__(self, interval, action):
+        self.interval = interval
+        self.action = action
+        self.stopEvent = threading.Event()
+        thread = threading.Thread(target=self.__setInterval)
         thread.start()
-    def __setInterval(self) :
-        nextTime=time.time()+self.interval
-        while not self.stopEvent.wait(nextTime - time.time()) :
-            nextTime+=self.interval
+
+    def __setInterval(self):
+        nextTime = time.time() + self.interval
+        while not self.stopEvent.wait(nextTime - time.time()):
+            nextTime += self.interval
             self.action()
-    def cancel(self) :
+
+    def cancel(self):
         self.stopEvent.set()
 
 
 def get_json_url(url):
-    if '4channel' in url:
+    if "4channel" in url:
         return f"{url.replace('boards.4channel','a.4cdn')}.json"
     else:
         return f"{url.replace('boards.4chan','a.4cdn')}.json"
@@ -56,11 +61,17 @@ def get_json_url(url):
 
 def get_thread_title(url):
     thread_json_url = get_json_url(url)
-    print(bcolors.BOLD + bcolors.OKCYAN + 'Link of json file of the thread is:' + f'{thread_json_url}' + bcolors.ENDC)
+    print(
+        bcolors.BOLD
+        + bcolors.OKCYAN
+        + "Link of json file of the thread is:"
+        + f"{thread_json_url}"
+        + bcolors.ENDC
+    )
     r = requests.get(thread_json_url, verify=False)
     thread_json = r.json()
-    thread_title = thread_json['posts'][0]['semantic_url']
-    print(bcolors.BOLD + bcolors.OKCYAN + f'\n{thread_title}' + bcolors.ENDC)
+    thread_title = thread_json["posts"][0]["semantic_url"]
+    print(bcolors.BOLD + bcolors.OKCYAN + f"\n{thread_title}" + bcolors.ENDC)
     return thread_title
 
 
@@ -69,9 +80,14 @@ def create_img_folder(url, thread_title):
     global thread_number
     board = url.split("/")[3]
     thread_number = str(url.split("thread/")[1])
-    path = Path.joinpath(Path.home(), f'4chan/{board}/{thread_title}__{thread_number}')
+    path = Path.joinpath(Path.home(), f"4chan/{board}/{thread_title}__{thread_number}")
     Path(path).mkdir(parents=True, exist_ok=True)
-    print(bcolors.OKGREEN + bcolors.BOLD + f'\nPath of created folder is: {path}' + bcolors.ENDC)
+    print(
+        bcolors.OKGREEN
+        + bcolors.BOLD
+        + f"\nPath of created folder is: {path}"
+        + bcolors.ENDC
+    )
     return path
 
 
@@ -80,7 +96,12 @@ def download_thread_json(url, path):
     print(thread_json_url)
     try:
         wget.download(thread_json_url, str(path))
-        print(bcolors.OKGREEN + bcolors.BOLD + "\nThread's json downloaded successfully!" + bcolors.ENDC)
+        print(
+            bcolors.OKGREEN
+            + bcolors.BOLD
+            + "\nThread's json downloaded successfully!"
+            + bcolors.ENDC
+        )
     except Exception as e:
         print(e)
 
@@ -89,15 +110,16 @@ def remove_extension(word):
     global EXTENSIONS
     for ext in EXTENSIONS:
         if ext in word:
-            return word.replace(ext, '')
+            return word.replace(ext, "")
 
 
 def is_archived(url):
-    print('Checking if thread is archived...')
+    print("Checking if thread is archived...")
     r = requests.get(url, verify=False)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    thread404 = soup.find('div',{'class':'boxbar'})
-    if thread404:
+    soup = BeautifulSoup(r.content, "html.parser")
+    thread404 = soup.find("div", {"class": "boxbar"})
+    thread_closed = soup.find("div", {"class": "closed"})
+    if thread404 or thread_closed:
         return True
     return False
 
@@ -105,14 +127,14 @@ def is_archived(url):
 def monitor_thread():
     if DOWNLOADING:
         # if already is downloading thread ignore the check
-        print('Already downloading...')
+        print("Already downloading...")
         return
     elif is_archived(URL):
         # if is archived stop monitoring the thread
-        print('This thread is archived.')
+        print("This thread is archived.")
         THREAD_MONITORING.cancel()
     else:
-        print('Downloading thread')
+        print("Downloading thread")
         # download the update json of thread and the new media if it has any
         threading.Thread(target=download_all).start()
         # self.download_all(url)
@@ -125,13 +147,13 @@ def download_all():
     DOWNLOADING = True
 
     r = requests.get(url, verify=False)
-    soup = BeautifulSoup(r.content, 'html.parser')
+    soup = BeautifulSoup(r.content, "html.parser")
     divMedia = soup.find_all("div", {"class": "fileText"})
     # Download a json of the threads
     try:
-        webpage_path = Path.joinpath(path, 'webpage')
+        webpage_path = Path.joinpath(path, "webpage")
         Path(webpage_path).mkdir(parents=True, exist_ok=True)
-        thread_json_path = Path.joinpath(webpage_path, 'thread.json')
+        thread_json_path = Path.joinpath(webpage_path, "thread.json")
         download_thread_json(url, thread_json_path)
     except Exception as e:
         print(e)
@@ -151,12 +173,23 @@ def download_all():
         for media in hrefTag:
             try:
                 generated_filename = media["href"].split("/")[-1].strip()
-                filename = f'{remove_extension(generated_filename)}__{media.text.strip()}'
+                filename = (
+                    f"{remove_extension(generated_filename)}__{media.text.strip()}"
+                )
                 if not os.path.isfile(f"{path}/{filename}"):
-                    print(bcolors.OKGREEN + bcolors.BOLD + f"\nDownloading {filename}" + bcolors.ENDC)
-                    wget.download(f'https:{media["href"]}', f'{path}/{filename}')
+                    print(
+                        bcolors.OKGREEN
+                        + bcolors.BOLD
+                        + f"\nDownloading {filename}"
+                        + bcolors.ENDC
+                    )
+                    wget.download(f'https:{media["href"]}', f"{path}/{filename}")
                 else:
-                    print(bcolors.WARNING + "\nThis file already exists, continuing..." + bcolors.ENDC)
+                    print(
+                        bcolors.WARNING
+                        + "\nThis file already exists, continuing..."
+                        + bcolors.ENDC
+                    )
             except Exception as e:
                 print(e)
                 print("\nFailed to download.")
@@ -166,11 +199,13 @@ def download_all():
 
 def main():
     global URL, PATH
-    URL = input(bcolors.HEADER + bcolors.BOLD + "Insert the thread link here: " + bcolors.ENDC)
+    URL = input(
+        bcolors.HEADER + bcolors.BOLD + "Insert the thread link here: " + bcolors.ENDC
+    )
     try:
         thread_title = get_thread_title(URL)
     except:
-        thread_title = URL.split('/')[1].split('.')[0]
+        thread_title = URL.split("/")[1].split(".")[0]
     PATH = create_img_folder(URL, thread_title)
     # download_all(url, path)
     # monitor_thread(url, path)
